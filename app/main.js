62a835d6aefe2833bb7b73f13133da6c8059f70a
youzi <3055351149@.qq.com>
2017-11-01 18:11:49 +0800

    oo

--- app/main.js
+++ app/main.js
@@ -1,5 +1,26 @@
-import App from './Greeter.js';
-import React from 'react';
-import {render} from 'react-dom';
 
-render(<App />,document.getElementById('root'))
+import Login from './login/index';
+import ListItem from './component/listItem';
+import Result from './login/result';
+import Head from './head';
+import Record from './component/record';
+import React,{Component} from 'react';
+import {render} from 'react-dom';
+import {Route,BrowserRouter,Link,IndexR} from 'react-router-dom';
+import {Button} from 'antd-mobile';
+
+const App = () => (
+	<BrowserRouter>
+		<div>
+			<Route exact path="/" component={Home} />
+			<Route path="/login/index" component={Login} />
+			<Route path="/record/:types" component={Record} />
+			<Route path="/login/result" component={Result} />
+		</div>
+	</BrowserRouter>
+)               
+
+const Home = (props) => {console.log(props, 'home'); return <div>			
+<Link to="/">home</Link>
+<Link to="/login/index">about</Link><Head /><ListItem /></div>}
+render(<App/>, document.getElementById('root'))
